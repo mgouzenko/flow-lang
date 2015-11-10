773ab0a684535199a5023f1deca53cd8067e825e
Zachary Gleicher <zachgleicher@gmail.com>
2015-11-10 18:00:44 -0500

    update regression testing framework

--- testall.sh
+++ testall.sh
@@ -5,6 +5,20 @@ FLOWC="./flowc"
 # Set time limit for all operations
 ulimit -t 30
 
+globallog=testall.log
+rm -f $globallog
+error=0
+globalerror=0
+
+keep=0
+
+Usage() {
+    echo "Usage: testall.sh [options] [.flow files]"
+    echo "-k    Keep intermediate files"
+    echo "-h    Print this help"
+    exit 1
+}
+
 SignalError() {
     if [ $error -eq 0 ] ; then
   echo "FAILED"
@@ -13,29 +27,103 @@ SignalError() {
     echo "  $1"
 }
 
-Run() {
-    echo $* 1>&2
-    eval $* || {
-  SignalError "$1 failed on $*"
-  return 1
+# Compare <outfile> <reffile> <difffile>
+# Compares the outfile with reffile.  Differences, if any, written to difffile
+Compare() {
+    generatedfiles="$generatedfiles $3"
+    echo diff -b $1 $2 ">" $3 1>&2
+    diff -b "$1" "$2" > "$3" 2>&1 || {
+      SignalError "$1 differs"
+      echo "FAILED $1 differs from $2" 1>&2
     }
 }
 
-CheckGrammar() {
-  echo "#### Testing $1"
-  Run "$FLOWC" "-g" "<" $1 
+# Run <args>
+# Report the command, run it, and report any errors
+Run() {
+    echo $* 1>&2
+    eval $* || {
+      SignalError "$1 failed on $*"
+      return 1
+    }
 }
+
+Check() {
+    error=0
+    basename=`echo $1 | sed 's/.*\\///
+                             s/.flow//'`
+    reffile=`echo $1 | sed 's/.flow$//'`
+    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
+
+    echo -n "$basename..."
+
+    echo 1>&2
+    echo "###### Testing $basename" 1>&2
+
+    generatedfiles=""
+
+    # generatedfiles="$generatedfiles ${basename}.i.out" &&
+    # Run "$FLOWC" "-i" "<" $1 ">" ${basename}.i.out &&
+    # Compare ${basename}.i.out ${reffile}.out ${basename}.i.diff
+
+    generatedfiles="$generatedfiles ${basename}.c.out" &&
+    Run "$FLOWC" "-c" $1 ">" ${basename}.c.out &&
+    Compare ${basename}.c.out ${reffile}.out ${basename}.c.diff
+
+    # Report the status and clean up the generated files
+
+    if [ $error -eq 0 ] ; then
+      if [ $keep -eq 0 ] ; then
+        rm -f $generatedfiles
+      fi
+      echo "OK"
+      echo "###### SUCCESS" 1>&2
+    else
+      echo "###### FAILED" 1>&2
+      globalerror=$error
+    fi
+}
+
+make >> $globallog
+
+while getopts kdpsh c; do
+    case $c in
+  k) # Keep intermediate files
+      keep=1
+      ;;
+  h) # Help
+      Usage
+      ;;
+    esac
+done
+
+shift `expr $OPTIND - 1`
 
 if [ $# -ge 1 ]
 then
     files=$@
 else
-    files="tests/grammar/test-*.flow"
+    files="tests/test-*.flow"
 fi
 
-for file in $files 
+for file in $files
 do
-  CheckGrammar $file 
+    case $file in
+  *test-*)
+      Check $file 2>> $globallog
+      ;;
+  *fail-*)
+      CheckFail $file 2>> $globallog
+      ;;
+  *)
+      echo "unknown file type $file"
+      globalerror=1
+      ;;
+    esac
 done
+
+if [ $keep -eq 0 ] ; then
+  make clean >> $globallog
+fi
 
 exit $globalerror
