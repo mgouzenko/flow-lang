9f1518b54f80c5be5acf7814b4b7f8c30d5388b3
Zachary Gleicher <zachgleicher@gmail.com>
2015-11-12 00:04:19 -0500

    compile arrays

--- compile.ml
+++ compile.ml
@@ -22,7 +22,7 @@ let compile (program : program) =
                   "struct " ^ translate_type t ^ "_channel* "
               with Not_found -> raise (Failure("Channel not supported")))
       | Struct(id) -> "struct " ^ id
-      | Array(size, t) -> translate_type t
+      | Array(size, t) ->  translate_type t ^ "[" ^ string_of_int size ^ "]"
       | List(t) -> "wtf?" in
 
     let rec translate_expr (expr: expr) =
@@ -81,6 +81,8 @@ let compile (program : program) =
         | Assign(id, expr) -> id ^ "=" ^ translate_expr expr
         | FunctionCall(id, expr_list) -> id ^ "(" ^ expr_list_to_string expr_list ^ ")"
         | StructInitializer(dot_initializer_list) -> "TODO"
+        | ArrayInitializer(expr_list) -> "{" ^ expr_list_to_string expr_list ^ "}"
+        | ArrayElement(id, elm_expr) -> id ^ "[" ^ translate_expr expr ^ "]"
         | Noexpr -> ""
 
      in
