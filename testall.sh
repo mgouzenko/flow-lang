c6d91205bbd338223bc747b01dcfebdf33158ae3
Adam Chelminski <chelminski.adam@gmail.com>
2015-11-15 18:23:28 -0500

    Add gcc stderr to output

--- testall.sh
+++ testall.sh
@@ -73,9 +73,9 @@ Check() {
 
     generatedfiles="$generatedfiles ${basename}.c" &&
     Run "$FLOWC" "-c" $1 ">" ${basename}.c &&
-    echo "Flow to C:" &&
-    cat ${basename}.c &&
-    gcc ${basename}.c  &&
+    #echo "Flow to C:" &&
+    #cat ${basename}.c &&
+    gcc ${basename}.c  2>&1 &&
     ./a.out > ${basename}.c.out
     Compare ${basename}.c.out ${reffile}.out ${basename}.c.diff
 
