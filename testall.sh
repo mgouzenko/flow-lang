7df57774e769310d30b3b8536a68a6f5f5617141
Adam Chelminski <chelminski.adam@gmail.com>
2015-11-15 18:33:11 -0500

    change lpthread to pthread

--- testall.sh
+++ testall.sh
@@ -75,7 +75,7 @@ Check() {
     Run "$FLOWC" "-c" $1 ">" ${basename}.c &&
     #echo "Flow to C:" &&
     #cat ${basename}.c &&
-    gcc -lpthread ${basename}.c  2>&1 &&
+    gcc -pthread ${basename}.c  2>&1 &&
     ./a.out > ${basename}.c.out
     Compare ${basename}.c.out ${reffile}.out ${basename}.c.diff
 
