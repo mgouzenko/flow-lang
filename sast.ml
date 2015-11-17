23089e345bceda51fd034059cfeca2818ef6944f
Mitchell Gouzenko <mgouzenko@gmail.com>
2015-11-17 00:29:01 -0500

    finished variable declaration

--- sast.ml
+++ sast.ml
@@ -1,35 +1,63 @@
 open Ast;;
 
-(* semantically analyzed ast *)
+type s_dot_initializer = {
+    s_dot_initializer_id: string;
+    s_dot_initializer_val: typed_expr;
+}
 
 (* typed expression *)
-type texpr =
+and expr_details =
     TIntLiteral of int
   | TStringLiteral of string
   | TBoolLiteral of bool
   | TCharLiteral of char
   | TDoubleLiteral of float
-  | TStructInitializer of dot_initializer list * flow_type
-  | TArrayInitializer of texpr list * flow_type
-  | TArrayElement of string * texpr * flow_type
+  | TStructInitializer of s_dot_initializer list
+  | TArrayInitializer of typed_expr list
+  | TArrayElement of string * typed_expr
   | TId of string * flow_type
-  | TBinOp of texpr * bin_op * texpr * flow_type
-  | TUnaryOp of unary_op * texpr * flow_type
-  | TAssign of string * texpr * flow_type
-  | TFunctionCall of string * texpr list * flow_type
+  | TBinOp of typed_expr * bin_op * typed_expr
+  | TUnaryOp of unary_op * typed_expr
+  | TAssign of string * typed_expr
+  | TFunctionCall of string * typed_expr list
   | TNoexpr
 
-(* typed statements *)
-(*
-type tstmt =
-    TExpr of texpr * flow_type
-  | TBlock of tstmt list
-  | TReturn of texpr * flow_type
-  | TDeclaration of variable_declaration
-  | TIf of texpr * tstmt * tstmt
-  | TFor of texpr * texpr * texpr * tstmt
-  | TWhile of texpr * tstmt
-  | TContinue
-  | TBreak
-  | TPoison of texpr
-  *)
+and typed_expr = expr_details * flow_type
+
+type s_variable_declaration = {
+    s_declaration_type: flow_type;
+    s_declaration_id: string;
+    s_declaration_initializer: typed_expr;
+}
+
+type s_stmt =
+    SExpr of typed_expr
+  | SBlock of s_stmt list
+  | SReturn of typed_expr
+  | SDeclaration of s_variable_declaration
+  | SIf of typed_expr * s_stmt * s_stmt
+  | SFor of typed_expr * typed_expr * typed_expr * s_stmt
+  | SWhile of typed_expr * s_stmt
+  | SContinue
+  | SBreak
+  | SPoison of typed_expr
+
+type s_function_declaration = {
+    s_return_type: flow_type;
+    s_function_name: string;
+    s_arguments: s_variable_declaration list;
+    s_has_definition: bool;
+    s_body : s_stmt list;
+}
+
+type s_struct_declaration = {
+    s_struct_name: string;
+    s_struct_members: s_variable_declaration list;
+}
+
+type s_declaration =
+      SVarDecl of s_variable_declaration
+    | SFuncDecl of s_function_declaration
+    | SStructDecl of s_struct_declaration
+
+type s_ast = s_declaration list
--- semantic_analysis.ml
+++ semantic_analysis.ml
@@ -2,6 +2,8 @@ open Ast;;
 open Sast;;
 
 (* helper functions, TODO: need to be modularized, some used in printer.ml *) 
+
+
 let rec string_of_type = function
       Int ->  "int"
     | Float -> "float"
@@ -41,24 +43,34 @@ let rec string_of_op = function
 
 (* translate flow ast to s_ast *)
 
-type symbol_table = {
-    parent : symbol_table option;
-    variables : variable_decleration list;
+type symtab = {
+    parent : symtab option;
+    variables : variable_declaration list;
+}
+
+type function_entry = {
+    name: string;
+    param_types: flow_type list;
+    ret_type: flow_type;
 }
 
 type environment = {
     return_type : flow_type option;
-    scope : symbol_table;
+    symbol_table: symtab;
+    funcs : function_entry list;
 }
 
-let rec find_variable_type (scope : symbol_table) (name : string) : flow_type =
+let rec find_variable_decl (symbol_table: symtab) (name : string) : flow_type =
     try
-        let vdecl = List.find (fun (var_decl) -> var_decl.declaration_id = name) scope.variables
-        in vdecl.declaration_type
+        List.find (fun (var_decl) -> var_decl.declaration_id = name) symbol_table.variables
     with Not_found ->
-        match scope.parent with
+        match symbol_table.parent with
             Some(parent) -> find_variable_type parent name
           | _ -> raise Not_found
+
+let find_variable_type (symbol_table: symtab) (name : string) : flow_type =
+    let vdecl = find_variable_decl symbol_table name in
+    vdecl.declaration_type
 
 let type_of_texpr = function
       TIntLiteral(i) -> Int
@@ -87,7 +99,6 @@ let check_binop (e1 : texpr) (e2 : texpr) (op : bin_op) : texpr =
           | (_, _) -> raise (Error("operator " ^ string_of_binop op ^ " not"
             ^ " compatible with " ^ string_of_type t1 ^ " and " ^ string_of_type t2))
     | (_) -> raise (Error("finish checking binops"))
-        
 
 let rec check_expr (env : environment) (e : expr) : texpr =
     match e with
@@ -99,7 +110,7 @@ let rec check_expr (env : environment) (e : expr) : texpr =
     | Id(s) ->
         let t = 
             try
-                find_variable_type env.scope s
+                find_variable_type env.symbol_table s
             with Not_found -> raise (Error("Unrecognized identifier " ^ s))
         in TId(s, t)
     | BinOp(e1, op, e2) ->
@@ -115,3 +126,53 @@ let rec check_expr (env : environment) (e : expr) : texpr =
     | TFunctionCall of string * texpr list * flow_type
     | TNoexpr
     *)
+
+let check_variable_declaration (env: environment) (decl: variable_declaration) =
+    let expr_details, t = check_expr env decl.declaration_initializer in
+    if t = decl.declaration_type then
+        (try let _ =
+            (* Try to find the a local variable of the same name. If found, it's an error. *)
+            List.find
+            (fun (vdecl) -> vdecl.declaration_id = decl.declaration_id)
+            env.symbol_table.variables in
+        raise (Error("Variable " ^ decl.declaration_id ^ " already declared in local scope"))
+
+        (* If not found, add the declaration to the symbol table and return the new environment *)
+        with Not_found ->
+            let new_symbol_table = {env.symbol_table with variables = decl::symbol_table.variables } in
+            let new_env =  { env with symbol_table = new_symbol_table }
+            and s_var_decl = {
+                s_declaration_type = decl.declaration_type;
+                s_declaration_id = decl.declaration_id;
+                s_declaration_initializer = (expr_details, t) } in
+            (new_env, s_var_decl))
+    else raise (Error(decl.declaration_id ^ ": Declaration type does not match expression"))
+
+and check_function_declaration (env: environment) (decl: function_declaration) =
+    "hello"
+
+and check_struct_declaration (env: environment) (decl: struct_declaration) =
+    "hello"
+
+let check_declaration (env: environment) (decl: s_declaration) -> (environment, s_declaration) =
+    match decl with
+      VarDecl(vdecl) -> check_variable_declaration env vdecl
+    | FuncDecl(fdecl) -> check_function_declaration env fdecl
+    | StructDecl(sdecl) -> check_struct_declaration env sdecl
+
+let check_progam (prog: program) =
+    let env = {
+        return_type = None;
+        scope = { parent : None; variables : []; };
+        funcs : [];
+    } in
+
+    (* acc is the accumulator; it's a tuple of env, decl_list.
+     * the fold left builds the accumulator, threading the environment
+     * through the list of declarations. When the fold finishes, decl_list
+     * should be a built list of s_declarations *)
+    let _, decl_list = List.fold_left
+    (fun acc decl ->
+        let new_env, snode = check_declaration (fst acc) decl in
+        (new_env, snode::(snd acc)))
+    (env, []) program in decl_list
